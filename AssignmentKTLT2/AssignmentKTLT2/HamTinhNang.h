ce2c078b189b53c8099c4f0e017677628e95a0f0
Đăng Bảo <dangbaoit@gmail.com>
2017-05-27 13:52:46 +0700

    Tạo thư mục gốc AssignmentKTLT2

---
+++ AssignmentKTLT2/AssignmentKTLT2/HamTinhNang.h
@@ -0,0 +1,44 @@
+﻿#pragma once
+#include<string>
+using namespace std;
+
+class HamTinhNang
+{
+public:
+	HamTinhNang();
+	~HamTinhNang();
+public:
+	//Nhóm hàm thao tác thông thường
+	void timSach(string tenNguoiDungNhapVao);
+	void guiYeuCauMuonSach_ND();
+	void xemThongBao_ND(string tenUser);
+	void xemThongTin_ND(string tenUser);
+	void doiMatKhau_ND(string tenUser, string matKhauCu, string matKhauMoi);
+	//Nhóm hàm liên quan tới người dùng
+	void chinhSuaThongTin_ND(string tenUser, string ngaySinh, string CMND, string diaChi);
+	void xoaKhoiHeThong_ND(string tenUser);
+	void themTaiKhoan_ND(string tenUser);
+	void moVaKhoaTaiKhoan_ND(string tenUser);
+	void lapLaiMatKhau_ND(string tenUser);
+	void timKiemTaiKhoan_ND(string tenCanTimKiem);
+	//Nhóm hàm liên quan tới sách
+	void chinhSuaThongTin_S(string maSoISBN);
+	void xoaKhoiHeThong_S(string maSoISBN);
+	void themVaoSachDaCo_S(string maSoISBN, int soLuongThemVao);
+	void themVaoSachChuaCo_S(string maSoISBN, string tenSach, string tenTacGia, string thoiGianCapNhatGanNhat);
+	void timKiemSach_S(string tenNguoiDungNhap);
+	void ghiNhanTraSach_S(string tenUser, string maSoISBN);
+	void phatViQuaHanMuon_S(string tenUser);
+	void sachThatLac_S(string tenUser, string maSoISBNSachBiLac);
+	//hàm liên quan đến việc kiểm tra nhập dữ liệu
+	bool kiemTraUser(string userNguoiDungNhap); //Kiểm tra tính hợp lệ
+	bool kiemTraPasW(string passNguoiDungNhap);	//Kiểm tra tính hợp lệ
+	string bamMatKhau(string userNguoiDungNhap, string passNguoiDungNhap);
+	bool kiemTraCMND(string CMNDNhapVao);
+	//Các hàm liên quan hỗ trợ người dùng
+	void taoThongBaoMoi(string tenThongBao, string tenUser);
+	bool kiemHaiChuoiTuongDong(string chuoiNhap_1, string chuoiNhap_2); //Dùng cho khi đổi mk
+	int kiemTraChenhLechThoiGian(string thoiGian_1, string thoiGian_2);
+	bool kiemTraThoiGianHopLe(string thoiGian);
+};
+
