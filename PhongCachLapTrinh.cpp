a1e5d87971ec76500df4c44a8ef42dc9147a8334
Đăng Bảo <dangbaoit@gmail.com>
2017-05-28 12:29:26 +0700

    Thêm thông tin về "Phong cách lập trình"

---
+++ PhongCachLapTrinh.cpp
@@ -0,0 +1,39 @@
+﻿#include<iostream>
+using namespace std;
+
+
+int main() {
+	//PHONG CÁCH LẬP TRÌNH
+
+	/*Tên hàm, tên biến:
+	//+) Viết hoa chữ cái đầu tiên từ từ thứ 2 trở đi.
+	//+) Tên biến, tên hàm nên đặt sao cho thể hiện được mục đích của nó
+	// Ví dụ:
+	*/
+	void hoanDoiHaiSo(int soThuNhat, int soThuHai);
+	
+	/* Đối với tên lớp, cấu trúc thì viết hoa toàn bộ các chữ cái đầu.
+	// Ví dụ:
+	*/
+	class SinhVien
+	{
+	public:
+		SinhVien();
+		~SinhVien();
+	private:
+	};
+
+	/* Khi viết các khối lệnh thì dấu ngoặc nhọn thứ nhất để
+	// sát với tên khối.
+	// Ví dụ:
+	*/
+	for (int i = 0; i < 10; i++){
+	//Các lệnh
+	}
+
+	// Khi viết lệnh không nên viết quá dài. Mỗi dòng chỉ nên khoảng 90 kí tự
+	// Cách chú thích tốt nhất chính là code là chú thích. 
+	// Ví dụ hàm để hoán đổi 2 số thì nên viết tên hàm là hoanDoiHaiSo. Như vậy
+	// Khi dùng cấp phát động thì hãy nhớ xóa cấp phát.
+	return 0;
+}
